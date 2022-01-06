bd95e772bceb06f2078adb7928085ac27a4193b4
spoorthipujariadobe <spujari@adobe.com>
2022-01-06 02:19:55 -0800

    fix Android tests

--- ACPCore/Assets/Demo/Tests/NewTests/TestSuite.cs
+++ ACPCore/Assets/Demo/Tests/NewTests/TestSuite.cs
@@ -28,7 +28,7 @@ namespace Tests
         public IEnumerator Test_ExtensionVersion()
         {
             if (Application.platform == RuntimePlatform.Android) {
-                return AssertEqualResult("CoreExtensionVersion", "coreVersion - 1.5.3-UN identityVersion - 1.2.0 lifecycleVersion - 1.0.4 signalVersion - 1.0.2 "); 
+                return AssertEqualResult("CoreExtensionVersion", "coreVersion - 1.8.0-U identityVersion - 1.2.2 lifecycleVersion - 1.0.7 signalVersion - 1.0.2 "); 
             } else if (Application.platform == RuntimePlatform.IPhonePlayer) {
                 return AssertEqualResult("CoreExtensionVersion", "coreVersion - 2.9.4-U identityVersion - 2.5.1 lifecycleVersion - 2.2.1 signalVersion - 2.2.0 "); 
             } else {
@@ -65,7 +65,7 @@ namespace Tests
         [UnityTest]
         public IEnumerator Test_GetIdentifiers()
         {
-            InvokeButtonClick("AppendToUrl");
+            InvokeButtonClick("SyncIdentifier");
             return AssertGreaterLengthResult("GetIdentifiers", "Ids is : ".Length);
         }
 
